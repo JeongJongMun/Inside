a037c4eb6159277fc02aa1922ecbff31c37419f0
girin-sj <radianttwilight24@gmail.com>
2023-08-08 01:40:47 +0900

    2023.08.08
    1. 로그인 화면, 회원가입 화면 bgm/버튼 눌렀을 때 효과음 추가(soundManager)

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource bgmSound;
+    public AudioClip[] bgmList;
+    
+
+    public static SoundManager instance = null;
+
+    void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    private void Awake() 
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+    
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        for(int i=0;i<bgmList.Length;i++) {
+            if(arg0.name == bgmList[i].name)
+                BgmSoundPlay(bgmList[i]);
+        }
+    }
+
+    // 효과음 재생
+    public void SFXPlay(string sfxName, AudioClip clip)
+    {
+        GameObject go = new GameObject(sfxName + "Sound");
+        AudioSource audiosource = go.AddComponent<AudioSource>();
+        audiosource.clip = clip;
+        audiosource.Play();
+
+        Destroy(go, clip.length);
+    }
+
+    // 배경음악 재생
+    public void BgmSoundPlay(AudioClip clip)
+    {
+        bgmSound.clip = clip;
+        bgmSound.loop = true;
+        bgmSound.volume = 0.1f;
+        bgmSound.Play();
+    }
+}
