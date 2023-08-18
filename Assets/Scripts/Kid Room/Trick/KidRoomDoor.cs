4be8bb6edceca4a3c44fd5d4f2d3e802fd1a72d5
girin-sj <radianttwilight24@gmail.com>
2023-08-19 00:29:46 +0900

    2023.08.19
    1. 아이방 효과음 추가(서랍장, 지도 자름, 문잠김, 램프)

--- Assets/Scripts/Kid Room/Trick/KidRoomDoor.cs
+++ Assets/Scripts/Kid Room/Trick/KidRoomDoor.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class KidRoomDoor : Trick
 {
+    public AudioClip doorClosedClip;
+
     public override void TrySolve(GameObject obj)
     {
         if (obj.name == gameObject.name)
         {
+            SoundManager.instance.SFXPlay("DoorClosed", doorClosedClip);
             Debug.Log("문 잠김 사운드 재생");
         }
     }
